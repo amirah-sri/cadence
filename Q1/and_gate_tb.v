d96b9d2fbf19089a03718cc3db6126631c4f0ff2
SWAVAGYASHREE <67193976+amirah-sri@users.noreply.github.com>
2024-07-30 07:58:01 +0530

    Add files via upload

---
+++ Q1/and_gate_tb.v
@@ -0,0 +1,34 @@
+module and_gate_tb;
+reg a;
+reg b;
+reg c;
+reg d;
+wire y;
+xnorgate g1( 
+.a(a),
+.b(b),
+.c(c),
+.d(d),
+.y(y));
+
+initial 
+begin
+a=0;b=0;c=0;d=0; #10;
+a=0;b=0;c=0;d=1; #10;
+a=0;b=0;c=1;d=0; #10;
+a=0;b=0;c=1;d=1; #10;
+a=0;b=1;c=0;d=0; #10;
+a=0;b=1;c=0;d=1; #10;
+a=0;b=1;c=1;d=0; #10;
+a=0;b=1;c=1;d=1; #10;
+a=1;b=0;c=0;d=0; #10;
+a=1;b=0;c=0;d=1; #10;
+a=1;b=0;c=1;d=0; #10;
+a=1;b=0;c=1;d=1; #10;
+a=1;b=1;c=0;d=0; #10;
+a=1;b=1;c=0;d=1; #10;
+a=1;b=1;c=1;d=0; #10;
+a=1;b=1;c=1;d=1; #10;
+$stop;
+end 
+endmodule
